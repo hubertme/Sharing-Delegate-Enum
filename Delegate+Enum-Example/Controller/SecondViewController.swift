530a3095e37a991b4b118177fb3f497779dcc99b
Hubert Wang <hubert.tatra@gmail.com>
2019-03-14 22:48:07 +0700

    Add SecondViewController

--- Delegate+Enum-Example/Controller/SecondViewController.swift
+++ Delegate+Enum-Example/Controller/SecondViewController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  SecondViewController.swift
 //  Delegate+Enum-Example
 //
 //  Created by Hubert Wang on 14/03/19.
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class FirstViewController: UIViewController {
+class SecondViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -16,4 +16,3 @@ class FirstViewController: UIViewController {
     }
 
 }
-
