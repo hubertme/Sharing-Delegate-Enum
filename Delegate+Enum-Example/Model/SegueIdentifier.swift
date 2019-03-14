add04daaae6fb192b82c1c7ab37419e97b13fd94
Hubert Wang <hubert.tatra@gmail.com>
2019-03-14 22:50:25 +0700

    Add segue identifier enum

---
+++ Delegate+Enum-Example/Model/SegueIdentifier.swift
@@ -0,0 +1,14 @@
+//
+//  SegueIdentifier.swift
+//  Delegate+Enum-Example
+//
+//  Created by Hubert Wang on 14/03/19.
+//  Copyright © 2019 Hubert Wang. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+enum SegueIdentifier: String {
+    case firstToSecond = "segueFirstToSecond"
+}
