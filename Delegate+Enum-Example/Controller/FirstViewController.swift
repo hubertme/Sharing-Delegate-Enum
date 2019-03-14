f1ddb8c1b83ef3424b94c213b2cf7d7554603e3d
Hubert Wang <hubert.tatra@gmail.com>
2019-03-14 22:45:16 +0700

    Change class name to FirstViewController

---
+++ Delegate+Enum-Example/Controller/FirstViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Delegate+Enum-Example
+//
+//  Created by Hubert Wang on 14/03/19.
+//  Copyright © 2019 Hubert Wang. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+
+
+}
+
